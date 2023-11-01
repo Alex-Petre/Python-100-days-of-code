fb9efc9b23520d0d4e03a0464f070262a121e892
Alexandru Petre <petre.p.alexandru@gmail.com>
2023-11-01 13:03:09 +0200

    Update enemies count to 2 in increase_enemies function

--- day-12.py
+++ day-12.py
@@ -8,3 +8,15 @@
 
 # increase_enemies()
 # print(f"enemies outside function: {enemies}")
+
+#############NOT BEST PRACTICE#####################
+enemies = 1
+
+def increase_enemies():
+  global enemies
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+#############NOT BEST PRACTICE#####################
