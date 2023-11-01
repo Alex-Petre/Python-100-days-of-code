6f0a8ed7349bf25236af4ac469047cefc94def38
Alexandru Petre <petre.p.alexandru@gmail.com>
2023-11-01 13:03:51 +0200

    Enhance increase_enemies function to return updated enemy count

--- day-12.py
+++ day-12.py
@@ -20,3 +20,12 @@ def increase_enemies():
 increase_enemies()
 print(f"enemies outside function: {enemies}")
 #############NOT BEST PRACTICE#####################
+enemies = 1
+
+def increase_enemies():
+  print(f"enemies inside function: {enemies}")
+  return enemies + 1
+
+enemies = increase_enemies()
+
+print(f"enemies outside function: {enemies}")
