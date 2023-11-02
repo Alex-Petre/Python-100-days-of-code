38d5ec952e4ba435f08163f6a2f2ffff3542bc7b
Alexandru Petre <petre.p.alexandru@gmail.com>
2023-11-02 12:20:27 +0200

    Added code to determine the generation based on the user's year of birth.

--- day-13.py
+++ day-13.py
@@ -8,7 +8,15 @@
 # my_function()
 
 # # Reproduce the Bug
-from random import randint
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(0, 5)
-print(dice_imgs[dice_num])
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+year = int(input("What's your year of birth? "))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
+
