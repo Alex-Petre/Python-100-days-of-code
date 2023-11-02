7975f7e29366cde977991925d3a9ec1bc2cfc3e8
Alexandru Petre <petre.p.alexandru@gmail.com>
2023-11-02 12:26:15 +0200

    Bug Fix - Revised code to correctly handle age input and provide the appropriate driving eligibility message.

--- day-13.py
+++ day-13.py
@@ -21,6 +21,6 @@
 #   print("You are a Gen Z.")
 
 # # Fix the Errors
-age = input("How old are you?")
-if age > 18:
-  print("You can drive at age {age}.")
+age = int(input("How old are you?"))
+if age >= 18:
+  print(f"You can drive at age {age}.")
