0e534a70b7bfe2c3f814ddee482ac2aa99e8eca3
Alexandru Petre <petre.p.alexandru@gmail.com>
2023-11-02 15:26:11 +0200

    Committed initial code for the two exercises.

--- day-13.py
+++ day-13.py
@@ -34,11 +34,43 @@
 # print(total_words)
 
 # #Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-    b_list.append(new_item)
-  print(b_list)
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#     b_list.append(new_item)
+#   print(b_list)
 
-mutate([1,2,3,5,8,13])
+# mutate([1,2,3,5,8,13])
+
+
+# number = int(input("Which number do you want to check? "))
+
+# if number % 2 == 0: # modified = with == to Fix Bug
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# year = int(input("Which year do you want to check?")) # Updated the 'year' variable to correctly accept an integer input.
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+
+for number in range(1, 101):
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  if number % 3 == 0:
+    print("Fizz")
+  if number % 5 == 0:
+    print("Buzz")
+  else:
+    print([number])
