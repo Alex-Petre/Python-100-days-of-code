53c9af703890ab6b04cf016611195f1aaf16294e
Alexandru Petre <petre.p.alexandru@gmail.com>
2023-11-02 12:37:01 +0200

    Bug Fix -Updated code to correctly calculate and display the total number of words based on user input.

--- day-13.py
+++ day-13.py
@@ -29,6 +29,6 @@
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
